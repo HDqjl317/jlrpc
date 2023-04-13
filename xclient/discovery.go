7be9b8977a442a4f0698d4b11b3e7ca4b7b499e0
qjl <jiale_quan@ustc.edu>
2023-04-13 17:15:44 +0800

    load-balance

---
+++ xclient/discovery.go
@@ -0,0 +1,92 @@
+/*
+ * @Author: jiale_quan jiale_quan@ustc.edu
+ * @Date: 2023-04-13 16:10:48
+ * @LastEditTime: 2023-04-13 17:11:44
+ * @Description:
+ * Copyright © jiale_quan, All Rights Reserved
+ */
+package xclient
+
+import (
+	"errors"
+	"math"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+type SelectMode int
+
+const (
+	RandomSelect SelectMode = iota
+	RoundRoubinSelect
+)
+
+type Discovery interface {
+	Refresh() error // refresh from remote registry
+	Update(servers []string) error
+	Get(mode SelectMode) (string, error)
+	GetAll() ([]string, error)
+}
+
+var _ Discovery = (*MultiServersDiscovery)(nil)
+
+type MultiServersDiscovery struct {
+	r       *rand.Rand
+	mu      sync.RWMutex
+	servers []string
+	index   int
+}
+
+func NewMultiserverDiscovery(servers []string) *MultiServersDiscovery {
+	d := &MultiServersDiscovery{
+		servers: servers,
+		r:       rand.New(rand.NewSource(time.Now().UnixNano())),
+	}
+	d.index = d.r.Intn(math.MaxInt32 - 1)
+	return d
+}
+
+func (d *MultiServersDiscovery) Refresh() error {
+	return nil
+}
+
+func (d *MultiServersDiscovery) Update(servers []string) error {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	d.servers = servers
+	return nil
+}
+
+/*
+ * Get a server according to mode
+ */
+func (d *MultiServersDiscovery) Get(mode SelectMode) (string, error) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	n := len(d.servers)
+	if n == 0 {
+		return "", errors.New("rpc discovery: no available servers")
+	}
+	switch mode {
+	case RandomSelect:
+		return d.servers[d.r.Intn(n)], nil
+	case RoundRoubinSelect:
+		s := d.servers[d.index%n]
+		d.index = (d.index + 1) % n
+		return s, nil
+	default:
+		return "", errors.New("rpc discovery: not supported select mode")
+	}
+}
+
+/*
+ * ruturn all servers in discovery
+ */
+func (d *MultiServersDiscovery) GetAll() ([]string, error) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	servers := make([]string, len(d.servers), len(d.servers))
+	copy(servers, d.servers)
+	return servers, nil
+}
